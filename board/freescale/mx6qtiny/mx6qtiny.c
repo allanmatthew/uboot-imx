3dff3df84f32f75be078c96ede5d1e8b52d9d9b9
Allan Matthew <amatthew@3dr.com>
2015-05-19 10:10:47 -0700

    SPI-NOR pins, disabled pmic

--- board/freescale/mx6qtiny/mx6qtiny.c
+++ board/freescale/mx6qtiny/mx6qtiny.c
@@ -68,22 +68,22 @@ DECLARE_GLOBAL_DATA_PTR;
 #define EPDC_PAD_CTRL    (PAD_CTL_PKE | PAD_CTL_SPEED_MED |	\
 	PAD_CTL_DSE_40ohm | PAD_CTL_HYS)
 
-#ifdef CONFIG_SYS_I2C_MXC
-#define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
-/* I2C2 Camera, MIPI, pfuze */
-struct i2c_pads_info i2c_pad_info1 = {
-	.scl = {
-		.i2c_mode = MX6_PAD_KEY_COL3__I2C2_SCL | PC,
-		.gpio_mode = MX6_PAD_KEY_COL3__GPIO4_IO12 | PC,
-		.gp = IMX_GPIO_NR(4, 12)
-	},
-	.sda = {
-		.i2c_mode = MX6_PAD_KEY_ROW3__I2C2_SDA | PC,
-		.gpio_mode = MX6_PAD_KEY_ROW3__GPIO4_IO13 | PC,
-		.gp = IMX_GPIO_NR(4, 13)
-	}
-};
-#endif
+//#ifdef CONFIG_SYS_I2C_MXC
+//#define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
+///* I2C2 Camera, MIPI, pfuze */
+//struct i2c_pads_info i2c_pad_info1 = {
+//	.scl = {
+//		.i2c_mode = MX6_PAD_KEY_COL3__I2C2_SCL | PC,
+//		.gpio_mode = MX6_PAD_KEY_COL3__GPIO4_IO12 | PC,
+//		.gp = IMX_GPIO_NR(4, 12)
+//	},
+//	.sda = {
+//		.i2c_mode = MX6_PAD_KEY_ROW3__I2C2_SDA | PC,
+//		.gpio_mode = MX6_PAD_KEY_ROW3__GPIO4_IO13 | PC,
+//		.gp = IMX_GPIO_NR(4, 13)
+//	}
+//};
+//#endif
 
 int dram_init(void)
 {
@@ -126,18 +126,18 @@ static void setup_iomux_enet(void)
 	gpio_set_value(IMX_GPIO_NR(1, 25), 1);
 }
 
-iomux_v3_cfg_t const usdhc2_pads[] = {
-	MX6_PAD_SD2_CLK__SD2_CLK	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD2_CMD__SD2_CMD	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD2_DAT0__SD2_DATA0	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD2_DAT1__SD2_DATA1	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD2_DAT2__SD2_DATA2	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD2_DAT3__SD2_DATA3	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_NANDF_D4__SD2_DATA4	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_NANDF_D5__SD2_DATA5	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_NANDF_D6__SD2_DATA6	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_NANDF_D7__SD2_DATA7	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_NANDF_D2__GPIO2_IO02	| MUX_PAD_CTRL(NO_PAD_CTRL), /* CD */
+iomux_v3_cfg_t const usdhc1_pads[] = {
+	MX6_PAD_SD1_CLK__SD1_CLK   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_CMD__SD1_CMD   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DAT0__SD1_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DAT1__SD1_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DAT2__SD1_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DAT3__SD1_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+        MX6_PAD_NANDF_D0__SD1_DATA4 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+        MX6_PAD_NANDF_D1__SD1_DATA5 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+        MX6_PAD_NANDF_D2__SD1_DATA6 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+        MX6_PAD_NANDF_D3__SD1_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD2_CLK__GPIO1_IO10 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
 iomux_v3_cfg_t const usdhc3_pads[] = {
@@ -150,31 +150,18 @@ iomux_v3_cfg_t const usdhc3_pads[] = {
 	MX6_PAD_SD3_RST__GPIO7_IO08 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
-iomux_v3_cfg_t const usdhc4_pads[] = {
-	MX6_PAD_SD4_CLK__SD4_CLK   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_CMD__SD4_CMD   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT0__SD4_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT1__SD4_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT2__SD4_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT3__SD4_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT4__SD4_DATA4 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT5__SD4_DATA5 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT6__SD4_DATA6 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-	MX6_PAD_SD4_DAT7__SD4_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
-};
-
 #ifdef CONFIG_SYS_USE_SPINOR
 iomux_v3_cfg_t const ecspi1_pads[] = {
-	MX6_PAD_KEY_COL0__ECSPI1_SCLK | MUX_PAD_CTRL(SPI_PAD_CTRL),
-	MX6_PAD_KEY_COL1__ECSPI1_MISO | MUX_PAD_CTRL(SPI_PAD_CTRL),
-	MX6_PAD_KEY_ROW0__ECSPI1_MOSI | MUX_PAD_CTRL(SPI_PAD_CTRL),
-	MX6_PAD_KEY_ROW1__GPIO4_IO09 | MUX_PAD_CTRL(NO_PAD_CTRL),
+	MX6_PAD_EIM_D18__ECSPI1_MOSI | MUX_PAD_CTRL(SPI_PAD_CTRL),
+	MX6_PAD_EIM_D17__ECSPI1_MISO | MUX_PAD_CTRL(SPI_PAD_CTRL),
+	MX6_PAD_EIM_D16__ECSPI1_SCLK | MUX_PAD_CTRL(SPI_PAD_CTRL),
+	MX6_PAD_EIM_EB2__GPIO2_IO30 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
 static void setup_spinor(void)
 {
 	imx_iomux_v3_setup_multiple_pads(ecspi1_pads, ARRAY_SIZE(ecspi1_pads));
-	gpio_direction_output(IMX_GPIO_NR(4, 9), 0);
+	gpio_direction_output(IMX_GPIO_NR(2,30), 0);
 }
 #endif
 
@@ -250,134 +237,134 @@ static void setup_iomux_uart(void)
 #ifdef CONFIG_SYS_I2C_MXC
 
 /* set all switches APS in normal and PFM mode in standby */
-static int setup_pmic_mode(int chip)
-{
-	unsigned char offset, i, switch_num, value;
+//static int setup_pmic_mode(int chip)
+//{
+//	unsigned char offset, i, switch_num, value;
+//
+//	if (!chip) {
+//		/* pfuze100 */
+//		switch_num = 6;
+//		offset = 0x31;
+//	} else {
+//		/* pfuze200 */
+//		switch_num = 4;
+//		offset = 0x38;
+//	}
+//
+//	value = 0xc;
+//	if (i2c_write(0x8, 0x23, 1, &value, 1)) {
+//		printf("Set SW1AB mode error!\n");
+//		return -1;
+//	}
+//
+//	for (i = 0; i < switch_num - 1; i++) {
+//		if (i2c_write(0x8, offset + i * 7, 1, &value, 1)) {
+//			printf("Set switch%x mode error!\n", offset);
+//			return -1;
+//		}
+//	}
+//
+//	return 0;
+//}
 
-	if (!chip) {
-		/* pfuze100 */
-		switch_num = 6;
-		offset = 0x31;
-	} else {
-		/* pfuze200 */
-		switch_num = 4;
-		offset = 0x38;
-	}
-
-	value = 0xc;
-	if (i2c_write(0x8, 0x23, 1, &value, 1)) {
-		printf("Set SW1AB mode error!\n");
-		return -1;
-	}
-
-	for (i = 0; i < switch_num - 1; i++) {
-		if (i2c_write(0x8, offset + i * 7, 1, &value, 1)) {
-			printf("Set switch%x mode error!\n", offset);
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
-static int setup_pmic_voltages(void)
-{
-	unsigned char value, rev_id = 0 ;
-
-	i2c_set_bus_num(1);
-	if (!i2c_probe(0x8)) {
-		if (i2c_read(0x8, 0, 1, &value, 1)) {
-			printf("Read device ID error!\n");
-			return -1;
-		}
-		if (i2c_read(0x8, 3, 1, &rev_id, 1)) {
-			printf("Read Rev ID error!\n");
-			return -1;
-		}
-		printf("Found PFUZE%s deviceid=%x,revid=%x\n",
-			((value & 0xf) == 0) ? "100" : "200", value, rev_id);
-
-		if (setup_pmic_mode(value & 0xf)) {
-			printf("setup pmic mode error!\n");
-			return -1;
-		}
-		/*For camera streaks issue,swap VGEN5 and VGEN3 to power camera.
-		*sperate VDDHIGH_IN and camera 2.8V power supply, after switch:
-		*VGEN5 for VDDHIGH_IN and increase to 3V to align with datasheet
-		*VGEN3 for camera 2.8V power supply
-		*/
-		/*increase VGEN3 from 2.5 to 2.8V*/
-		if (i2c_read(0x8, 0x6e, 1, &value, 1)) {
-			printf("Read VGEN3 error!\n");
-			return -1;
-		}
-		value &= ~0xf;
-		value |= 0xa;
-		if (i2c_write(0x8, 0x6e, 1, &value, 1)) {
-			printf("Set VGEN3 error!\n");
-			return -1;
-		}
-		/*increase VGEN5 from 2.8 to 3V*/
-		if (i2c_read(0x8, 0x70, 1, &value, 1)) {
-			printf("Read VGEN5 error!\n");
-			return -1;
-		}
-		value &= ~0xf;
-		value |= 0xc;
-		if (i2c_write(0x8, 0x70, 1, &value, 1)) {
-			printf("Set VGEN5 error!\n");
-			return -1;
-		}
-		/* set SW1AB staby volatage 0.975V*/
-		if (i2c_read(0x8, 0x21, 1, &value, 1)) {
-			printf("Read SW1ABSTBY error!\n");
-			return -1;
-		}
-		value &= ~0x3f;
-		value |= 0x1b;
-		if (i2c_write(0x8, 0x21, 1, &value, 1)) {
-			printf("Set SW1ABSTBY error!\n");
-			return -1;
-		}
-		/* set SW1AB/VDDARM step ramp up time from 16us to 4us/25mV */
-		if (i2c_read(0x8, 0x24, 1, &value, 1)) {
-			printf("Read SW1ABSTBY error!\n");
-			return -1;
-		}
-		value &= ~0xc0;
-		value |= 0x40;
-		if (i2c_write(0x8, 0x24, 1, &value, 1)) {
-			printf("Set SW1ABSTBY error!\n");
-			return -1;
-		}
-
-		/* set SW1C staby volatage 0.975V*/
-		if (i2c_read(0x8, 0x2f, 1, &value, 1)) {
-			printf("Read SW1CSTBY error!\n");
-			return -1;
-		}
-		value &= ~0x3f;
-		value |= 0x1b;
-		if (i2c_write(0x8, 0x2f, 1, &value, 1)) {
-			printf("Set SW1CSTBY error!\n");
-			return -1;
-		}
-
-		/* set SW1C/VDDSOC step ramp up time to from 16us to 4us/25mV */
-		if (i2c_read(0x8, 0x32, 1, &value, 1)) {
-			printf("Read SW1ABSTBY error!\n");
-			return -1;
-		}
-		value &= ~0xc0;
-		value |= 0x40;
-		if (i2c_write(0x8, 0x32, 1, &value, 1)) {
-			printf("Set SW1ABSTBY error!\n");
-			return -1;
-		}
-	}
-
-	return 0;
-}
+//static int setup_pmic_voltages(void)
+//{
+//	unsigned char value, rev_id = 0 ;
+//
+//	i2c_set_bus_num(1);
+//	if (!i2c_probe(0x8)) {
+//		if (i2c_read(0x8, 0, 1, &value, 1)) {
+//			printf("Read device ID error!\n");
+//			return -1;
+//		}
+//		if (i2c_read(0x8, 3, 1, &rev_id, 1)) {
+//			printf("Read Rev ID error!\n");
+//			return -1;
+//		}
+//		printf("Found PFUZE%s deviceid=%x,revid=%x\n",
+//			((value & 0xf) == 0) ? "100" : "200", value, rev_id);
+//
+//		if (setup_pmic_mode(value & 0xf)) {
+//			printf("setup pmic mode error!\n");
+//			return -1;
+//		}
+//		/*For camera streaks issue,swap VGEN5 and VGEN3 to power camera.
+//		*sperate VDDHIGH_IN and camera 2.8V power supply, after switch:
+//		*VGEN5 for VDDHIGH_IN and increase to 3V to align with datasheet
+//		*VGEN3 for camera 2.8V power supply
+//		*/
+//		/*increase VGEN3 from 2.5 to 2.8V*/
+//		if (i2c_read(0x8, 0x6e, 1, &value, 1)) {
+//			printf("Read VGEN3 error!\n");
+//			return -1;
+//		}
+//		value &= ~0xf;
+//		value |= 0xa;
+//		if (i2c_write(0x8, 0x6e, 1, &value, 1)) {
+//			printf("Set VGEN3 error!\n");
+//			return -1;
+//		}
+//		/*increase VGEN5 from 2.8 to 3V*/
+//		if (i2c_read(0x8, 0x70, 1, &value, 1)) {
+//			printf("Read VGEN5 error!\n");
+//			return -1;
+//		}
+//		value &= ~0xf;
+//		value |= 0xc;
+//		if (i2c_write(0x8, 0x70, 1, &value, 1)) {
+//			printf("Set VGEN5 error!\n");
+//			return -1;
+//		}
+//		/* set SW1AB staby volatage 0.975V*/
+//		if (i2c_read(0x8, 0x21, 1, &value, 1)) {
+//			printf("Read SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//		value &= ~0x3f;
+//		value |= 0x1b;
+//		if (i2c_write(0x8, 0x21, 1, &value, 1)) {
+//			printf("Set SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//		/* set SW1AB/VDDARM step ramp up time from 16us to 4us/25mV */
+//		if (i2c_read(0x8, 0x24, 1, &value, 1)) {
+//			printf("Read SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//		value &= ~0xc0;
+//		value |= 0x40;
+//		if (i2c_write(0x8, 0x24, 1, &value, 1)) {
+//			printf("Set SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//
+//		/* set SW1C staby volatage 0.975V*/
+//		if (i2c_read(0x8, 0x2f, 1, &value, 1)) {
+//			printf("Read SW1CSTBY error!\n");
+//			return -1;
+//		}
+//		value &= ~0x3f;
+//		value |= 0x1b;
+//		if (i2c_write(0x8, 0x2f, 1, &value, 1)) {
+//			printf("Set SW1CSTBY error!\n");
+//			return -1;
+//		}
+//
+//		/* set SW1C/VDDSOC step ramp up time to from 16us to 4us/25mV */
+//		if (i2c_read(0x8, 0x32, 1, &value, 1)) {
+//			printf("Read SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//		value &= ~0xc0;
+//		value |= 0x40;
+//		if (i2c_write(0x8, 0x32, 1, &value, 1)) {
+//			printf("Set SW1ABSTBY error!\n");
+//			return -1;
+//		}
+//	}
+//
+//	return 0;
+//}
 
 #ifdef CONFIG_LDO_BYPASS_CHECK
 void ldo_mode_set(int ldo_bypass)
@@ -496,10 +483,9 @@ void ldo_mode_set(int ldo_bypass)
 #endif
 
 #ifdef CONFIG_FSL_ESDHC
-struct fsl_esdhc_cfg usdhc_cfg[3] = {
-	{USDHC2_BASE_ADDR},
+struct fsl_esdhc_cfg usdhc_cfg[2] = {
+	{USDHC1_BASE_ADDR},
 	{USDHC3_BASE_ADDR},
-	{USDHC4_BASE_ADDR},
 };
 
 int mmc_get_env_devno(void)
@@ -531,8 +517,8 @@ int mmc_map_to_kernel_blk(int dev_no)
 	return dev_no + 1;
 }
 
-#define USDHC2_CD_GPIO	IMX_GPIO_NR(2, 2)
-#define USDHC3_CD_GPIO	IMX_GPIO_NR(2, 0)
+#define USDHC1_CD_GPIO	IMX_GPIO_NR(1, 10)
+#define USDHC3_CD_GPIO	IMX_GPIO_NR(7, 8)
 
 int board_mmc_getcd(struct mmc *mmc)
 {
@@ -540,14 +526,11 @@ int board_mmc_getcd(struct mmc *mmc)
 	int ret = 0;
 
 	switch (cfg->esdhc_base) {
-	case USDHC2_BASE_ADDR:
-		ret = !gpio_get_value(USDHC2_CD_GPIO);
+	case USDHC1_BASE_ADDR:
+		ret = !gpio_get_value(USDHC1_CD_GPIO);
 		break;
 	case USDHC3_BASE_ADDR:
 		ret = !gpio_get_value(USDHC3_CD_GPIO);
-		break;
-	case USDHC4_BASE_ADDR:
-		ret = 1; /* eMMC/uSDHC4 is always present */
 		break;
 	}
 
@@ -559,31 +542,19 @@ int board_mmc_init(bd_t *bis)
 	s32 status = 0;
 	int i;
 
-	/*
-	 * According to the board_mmc_init() the following map is done:
-	 * (U-boot device node)    (Physical Port)
-	 * mmc0                    SD2
-	 * mmc1                    SD3
-	 * mmc2                    eMMC
-	 */
 	for (i = 0; i < CONFIG_SYS_FSL_USDHC_NUM; i++) {
 		switch (i) {
 		case 0:
 			imx_iomux_v3_setup_multiple_pads(
-				usdhc2_pads, ARRAY_SIZE(usdhc2_pads));
-			gpio_direction_input(USDHC2_CD_GPIO);
-			usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC2_CLK);
+				usdhc1_pads, ARRAY_SIZE(usdhc1_pads));
+			gpio_direction_input(USDHC1_CD_GPIO);
+			usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
 			break;
 		case 1:
 			imx_iomux_v3_setup_multiple_pads(
 				usdhc3_pads, ARRAY_SIZE(usdhc3_pads));
 			gpio_direction_input(USDHC3_CD_GPIO);
 			usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
-			break;
-		case 2:
-			imx_iomux_v3_setup_multiple_pads(
-				usdhc4_pads, ARRAY_SIZE(usdhc4_pads));
-			usdhc_cfg[2].sdhc_clk = mxc_get_clock(MXC_ESDHC4_CLK);
 			break;
 		default:
 			printf("Warning: you configured more USDHC controllers"
@@ -1164,18 +1135,18 @@ static const struct boot_mode board_boot_modes[] = {
 
 int board_late_init(void)
 {
-	int ret = 0;
+//	int ret = 0;
 #ifdef CONFIG_CMD_BMODE
 	add_board_boot_modes(board_boot_modes);
 #endif
 
-#ifdef CONFIG_SYS_I2C_MXC
-	setup_i2c(1, CONFIG_SYS_I2C_SPEED,
-			0x7f, &i2c_pad_info1);
-	ret = setup_pmic_voltages();
-	if (ret)
-		return -1;
-#endif
+//#ifdef CONFIG_SYS_I2C_MXC
+// 	setup_i2c(1, CONFIG_SYS_I2C_SPEED,
+//			0x7f, &i2c_pad_info1);
+//	ret = setup_pmic_voltages();
+//	if (ret)
+//		return -1;
+//#endif
 
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
--- include/configs/mx6qtiny.h
+++ include/configs/mx6qtiny.h
@@ -12,6 +12,8 @@
 #include <asm/arch/imx-regs.h>
 #include <asm/imx-common/gpio.h>
 
+#define CONFIG_SYS_PROMPT "Tiny U-Boot > "
+
 #define CONFIG_MACH_TYPE	3980
 #define CONFIG_MXC_UART_BASE	UART1_BASE
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
@@ -31,7 +33,7 @@
 #define CONFIG_MXC_USB_FLAGS   0
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 1 /* Enabled USB controller number */
 
-#define CONFIG_SYS_FSL_USDHC_NUM	1
+#define CONFIG_SYS_FSL_USDHC_NUM	2
 #define CONFIG_SYS_MMC_ENV_DEV		1	/* SDHC3 */
 #define CONFIG_SYS_MMC_ENV_PART                0       /* user partition */
 
